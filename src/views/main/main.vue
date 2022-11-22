efeb3904e622411fa71131d1b5cd388ab3138789
Linxae <702611663@qq.com>
2022-11-22 20:15:10 +0800

    feat: init vuex

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,7 +1,12 @@
 <template>
     <div>main</div>
+    <div>{{ store.state.name }}</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useStore } from "vuex";
+
+const store = useStore();
+</script>
 
 <style scoped></style>
