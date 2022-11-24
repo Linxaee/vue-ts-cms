9a562670e26314d9d3c2b17e2e5be1a27dbe6911
Linxae <702611663@qq.com>
2022-11-24 17:06:50 +0800

    feat: configure global axios interceptors

--- src/service/requests/request.ts
+++ src/service/requests/request.ts
@@ -1,36 +1,40 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
-interface LinRequestInterceptor {
-    requestInterceptor: (config: AxiosRequestConfig) => AxiosRequestConfig;
-    requestInterceptorCatch: (error: any) => any;
-    responseInterceptor: (config: AxiosResponse) => AxiosResponse;
-    responseInterceptorCatch: (error: any) => any;
-}
-// 自定义config接口继承AxiosRequestConfig的基础上，扩展interceptors属性，其上有待实现的拦截器
-interface LinRequestConfig extends AxiosRequestConfig {
-    interceptors: LinRequestInterceptor;
-}
+import axios from "axios";
+import type { AxiosInstance } from "axios";
+import type { LinRequestInterceptor, LinRequestConfig } from "@/service/requests/types";
 class LinRequest {
     // 多个request对象时创建多个实例并保留
     instance: AxiosInstance;
-    // 可传入自定义拦截器
-    interceptors: LinRequestInterceptor;
+    // 可选传入自定义拦截器
+    interceptors?: LinRequestInterceptor;
 
     constructor(config: LinRequestConfig) {
         // 保留实例
         this.instance = axios.create(config);
         // 保留拦截器
         this.interceptors = config.interceptors;
-        // 请求拦截器
+        // 请求拦截器 ?可链式选
         this.instance.interceptors.request.use(
-            this.interceptors.requestInterceptor,
-            this.interceptors.requestInterceptorCatch
+            this.interceptors?.requestInterceptor,
+            this.interceptors?.requestInterceptorCatch
         );
         // 响应拦截器
-        this.instance.interceptors.response.use(
-            this.interceptors.responseInterceptor,
-            this.interceptors.responseInterceptorCatch
+        this.instance?.interceptors.response.use(
+            this.interceptors?.responseInterceptor,
+            this.interceptors?.responseInterceptorCatch
         );
+
+        // 全局请求拦截器
+        this.instance.interceptors.request.use((config) => {
+            console.log("全局请求拦截器");
+            return config;
+        });
+        // 全局响应拦截器
+        this.instance?.interceptors.response.use((err) => {
+            console.log("全局响应拦截器");
+            return err;
+        });
     }
+
     request(config: LinRequestConfig) {
         this.instance.request(config).then((res) => {
             console.log(res);
---
+++ src/service/requests/types.ts
@@ -0,0 +1,12 @@
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+// 自定义请求/响应拦截器接口
+export interface LinRequestInterceptor {
+    requestInterceptor: (config: AxiosRequestConfig) => AxiosRequestConfig;
+    requestInterceptorCatch: (error: any) => any;
+    responseInterceptor: (config: AxiosResponse) => AxiosResponse;
+    responseInterceptorCatch: (error: any) => any;
+}
+// 自定义config接口继承AxiosRequestConfig的基础上，扩展interceptors属性，其上有待实现的拦截器
+export interface LinRequestConfig extends AxiosRequestConfig {
+    interceptors?: LinRequestInterceptor;
+}
