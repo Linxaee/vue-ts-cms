40c3128ed1a3f66995ab81d23190c0c7f87ff2f4
Linxae <702611663@qq.com>
2022-12-01 01:13:23 +0800

    feat: 添加notfound

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,6 +18,11 @@ const routes: RouteRecordRaw[] = [
         path: "/main",
         name: "main",
         component: () => import("@/views/main/main.vue")
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notFound",
+        component: () => import("@/views/not-found/not-found.vue")
     }
 ];
 
